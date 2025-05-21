4df0c9b1690a1274b0a958e30acd6121acc27593
M-Unity <121330925+M-UnityDev@users.noreply.github.com>
2025-05-21 20:01:43 +0500

    0.95 ALPHA

--- hypr/change_theme.sh
+++ hypr/change_theme.sh
@@ -9,6 +9,8 @@ POSITIONAL_ARGS=()
 					ColorConfig="/ast/thm/ColorRed.css"
 					HyprPaperConfig="/ast/thm/HyprPaperRed.conf"
 					HyprLockConfig="/ast/thm/HyprRed.conf"
+					HyprCursorConfig="/ast/thm/HyprCursorRed.conf"
+					CursorTheme="Bibata-Original-Red"
 					Wall="/ast/spr/WallRed.png"
 					Logo="/ast/spr/LogoRed.png"
 					;;
@@ -17,6 +19,8 @@ POSITIONAL_ARGS=()
                     ColorConfig="/ast/thm/ColorGreen.css"
 					HyprPaperConfig="/ast/thm/HyprPaperGreen.conf"
 					HyprLockConfig="/ast/thm/HyprGreen.conf"
+					HyprCursorConfig="/ast/thm/HyprCursorGreen.conf"
+					CursorTheme="Bibata-Original-Green"
 					Wall="/ast/spr/WallGreen.png"
 					Logo="/ast/spr/LogoGreen.png"
                     ;;
@@ -25,6 +29,8 @@ POSITIONAL_ARGS=()
                     ColorConfig="/ast/thm/ColorBlue.css"
 					HyprPaperConfig="/ast/thm/HyprPaperBlue.conf"
 					HyprLockConfig="/ast/thm/HyprBlue.conf"
+					HyprCursorConfig="/ast/thm/HyprCursorBlue.conf"
+					CursorTheme="Bibata-Original-Blue"
 					Wall="/ast/spr/WallBlue.png"
 					Logo="/ast/spr/LogoBlue.png"
                     ;;
@@ -33,6 +39,8 @@ POSITIONAL_ARGS=()
                     ColorConfig="/ast/thm/ColorBlack.css"
 					HyprPaperConfig="/ast/thm/HyprPaperBlack.conf"
 					HyprLockConfig="/ast/thm/HyprBlack.conf"
+					HyprCursorConfig="/ast/thm/HyprCursorBlack.conf"
+					CursorTheme="Bibata-Original-Black"
 					Wall="/ast/spr/WallBlack.png"
 					Logo="/ast/spr/LogoBlack.png"
                     ;;
@@ -44,7 +52,8 @@ POSITIONAL_ARGS=()
 			;;
 	esac
 set -- "${POSITIONAL_ARGS[@]}"
-HomeDirectory="/home/m-unity/"
+HomeDirectory=$HOME
+hyprctl setcursor $CursorTheme 20
 hyprctl hyprpaper preload $Wall > /dev/null
 hyprctl hyprpaper wallpaper ,$Wall > /dev/null
 ln -sf $Logo /ast/spr/cur/LogoCurrent.png
@@ -52,6 +61,7 @@ cp -f $KittyConfig /ast/thm/cur/KittyCurrent.conf
 cp -f $ColorConfig /ast/thm/cur/ColorCurrent.css
 cp -f $HyprPaperConfig $HomeDirectory.config/hypr/hyprpaper.conf
 cp -f $HyprLockConfig /ast/thm/cur/HyprCurrent.conf
+cp -f $HyprCursorConfig /ast/thm/cur/HyprCursorCurrent.conf
 hyprshade on shake
 killall waybar
 kitty @ load-config
