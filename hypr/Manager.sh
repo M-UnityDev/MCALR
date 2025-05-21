4597fdb4132e30d7a5d79d8534b928832ecb27e2
M-Unity <mihailsheshukov10@gmail.com>
2025-05-21 23:05:45 +0500

    Update Manager.sh

--- hypr/Manager.sh
+++ hypr/Manager.sh
@@ -1,2 +1,17 @@
-#!/usr/bin/env bash
-[ $(date +%b) = "Dec" ] && hyprctl keyword decoration:shadow:enabled true || hyprctl keyword decoration:shadow:enabled false
+#!/bin/bash
+stop () {
+	hyprctl keyword decoration:shadow:enabled false 
+	exit
+}
+[[ $(date +%b) = "Dec" || $(date +%b) = "Jan" ]] && hyprctl keyword decoration:shadow:enabled true || stop
+coloractivenumber=$((($RANDOM%3)+1))
+colorinactivenumber=$((($RANDOM%3)+1))
+red=4284678144
+green=4278215424
+blue=4278190179
+[ $coloractivenumber = 1 ] && hyprctl keyword decoration:shadow:color $red
+[ $coloractivenumber = 2 ] && hyprctl keyword decoration:shadow:color $green
+[ $coloractivenumber = 3 ] && hyprctl keyword decoration:shadow:color $blue
+[ $colorinactivenumber = 1 ] && hyprctl keyword decoration:shadow:color_inactive $red
+[ $colorinactivenumber = 2 ] && hyprctl keyword decoration:shadow:color_inactive $green
+[ $colorinactivenumber = 3 ] && hyprctl keyword decoration:shadow:color_inactive $blue
